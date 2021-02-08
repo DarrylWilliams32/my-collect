8ad9fd46d5be8a044b85d29c25610ab9559da75b
darryl_williams <darrylwilliams6@gmail.com>
2021-02-07 19:12:41 -0500

    commit tree cc0d651dc31339c07f2c46abef4d78d0e22f1831

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,17 @@
+def my_collect
+empty_array = []
+counter = 0
+my_collect.each do |languages, students|
+counter += 1
+  end
+my collect
 
-
+#likely all going to be one method- should take in an argument of a 'collection' and iterate over those collections. 
+#following are all the failed tests
+#can handle an empty collection (FAILED - 1)
+#yields the correct element from a given collection, in this case languages (FAILED - 2)
+#returns a new collection of appropriately modified elements, in this case capitalized languages (FAILED - 3)
+#does not modify the original collection (FAILED - 4)
+#yields the correct element from the given collection, in this case students (FAILED - 5)
+#returns a new collection of appropriately modified elements, in this case student first names (FAILED - 6)
+#does not modify the original collection (FAILED - 7)
